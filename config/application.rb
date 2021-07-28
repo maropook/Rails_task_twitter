b2cd355c037332070f0df9e8bea56040eef4448b
maropook <ii440092@icloud.com>
2021-07-29 00:22:11 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module DeviseRais6Rebange
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
+
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,17 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => 'https://timory.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://timory.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV["SENDGRID_USERNAME"],
+      :password => ENV["SENDGRID_PASSWORD"],
+      :domain => "heroku.com",
+      :address => "smtp.sendgrid.net",
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 
 end
