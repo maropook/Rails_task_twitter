5dd3391a4ff1a38164074a7651e3703327cb9af6
maropook <ii440092@icloud.com>
2021-07-29 00:29:53 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.default_url_options = { :host => 'https://timory.herokuapp.com' }
-  config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
+  config.action_mailer.raise_delivery_errors = false #この一文も追記!!
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :user_name => ENV["SENDGRID_USERNAME"],
